612b9069a2a07dd83deff83e2476fe1a406108ac
Kelton-wlysses <kelton.dev@gmail.com>
2024-11-07 14:14:32 -0300

    aula 01

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcAcacianoNeves.Models;
+
+namespace MvcAcacianoNeves.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        HomeModel home = new HomeModel();
+
+        home.Nome = "Kelton Wlysses";
+        home.Email = "kelton.wlysses@mail.com";
+
+        return View(home);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcAcacianoNeves.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
